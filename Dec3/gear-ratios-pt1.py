aed60bec9044bb75e3166b04e217f8c932ddf462
alejandraavitia03 <aleavitia03@gmail.com>
2023-12-21 15:08:00 -0800

    maybe a def

--- Dec3/gear-ratios-pt1.py
+++ Dec3/gear-ratios-pt1.py
@@ -10,6 +10,7 @@ parts = [list(p) for p in stripped_parts]
 
 row = len(parts)
 col = len(parts[0])
+
 symbol_locations = []
 seen = []
 
@@ -20,29 +21,34 @@ for r in range(row):
     for c in range(col):
         if parts[r][c] in special_characters:
             symbol_locations.append([r, c])
+#print(symbol_locations)
 
 
 for loc in symbol_locations:
     currX = loc[0]
     currY = loc[1]
     print("For symbol: ", parts[currX][currY])
-    for d in directions:
+    for d in directions: #[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, 1], [1, -1], [-1, -1]
         x = currX + d[0]
         y = currY + d[1]
 
         if parts[x][y].isdigit():
-            seen[x][y] = True
+            #Call a function that gets the number 
+            # num_to_add = findNum(parts[x], y)
+            seen[x][y] = True 
 
             print("a nummber!: ", parts[x][y], )
     print('\n')
 # print(seen)
-# print('\n'.join(map(''.join, parts)))
+print('\n'.join(map(''.join, parts)))
 
 
-def findNum(row, i, j) -> int:
+def findNum(row, i) -> int:
     start_i = 0
     end_i = 0
     num = " "
+    while start_i < y:
+        if row[]
     for r in row:
         while end_i < len(row) and row[end_i].isdigit():
             end_i += 1
